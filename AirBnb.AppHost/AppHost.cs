d9bacbb766e517a58913cae5760f0b5c53abdf88
Robinson <robinsonmoreira2004@gmail.com>
2026-06-22 22:23:57 -0500

    Agregar soporte para PostgreSQL en la configuración del proyecto y crear la clase Departamento en el dominio; eliminar clase innecesaria.

--- AirBnb.AppHost/AppHost.cs
+++ AirBnb.AppHost/AppHost.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var dbServer = builder.AddPostgres("dbserver").WithLifetime(ContainerLifetime.Persistent);
+dbServer.WithPgAdmin(optins => 
+    optins.WithHostPort(5555).WithLifetime(ContainerLifetime.Persistent)
+);
+
+var db = dbServer.AddDatabase("airbnbdb");
+
+builder.AddProject<Projects.Api>("Api").WithExternalHttpEndpoints().WithReference(db).WaitFor(db);
+
 builder.Build().Run();
