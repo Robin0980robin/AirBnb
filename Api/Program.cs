2568c7e8ec62c1de7aaf82f7d6dad59170400517
Robinson <robinsonmoreira2004@gmail.com>
2026-06-22 23:06:11 -0500

    Agregar implementación de la funcionalidad de gestión de departamentos; incluir repositorio y consultas para obtener todos los departamentos.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,15 @@
+using Application.Departamentos.Queries;
+using Domain;
+using Infrastructure.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IDepartamentoGetAll, DepartamentoGetAll>();
+builder.Services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
 
 var app = builder.Build();
 
@@ -12,42 +19,17 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-};
-
 app.MapGet(
-        "/weatherforecast",
-        () =>
+        "/departamentos",
+        async (IDepartamentoGetAll departamentoGetAll) =>
         {
-            var forecast = Enumerable
-                .Range(1, 5)
-                .Select(index => new WeatherForecast(
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
+            var departamentos = await departamentoGetAll.Execute();
+            return Results.Ok(departamentos);
         }
     )
-    .WithName("GetWeatherForecast");
+    .WithName("GetDepartamentos");
+
+app.UseHttpsRedirection();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
